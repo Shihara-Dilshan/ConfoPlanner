9b1a4fd340e48ae33ce2a2ef4405f338f51407bf
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-06-01 15:43:55 +0530

    added proper react manual setup

--- FontEnd/src/App.js
+++ FontEnd/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "App.css";
 
-const App = () => {
-  return <div>sdsdsd</div>;
-};
+import Nav from "./Nav";
+
+const App = () => (
+  <div>
+    <Nav />
+    <h1 className="wow">Hello React!!!!!</h1>
+  </div>
+);
 
 export default App;

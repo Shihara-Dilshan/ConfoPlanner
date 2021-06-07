87b9c1f6b9598aee7c147a9a556213921459134d
pramitha1166 <pramitha.wanigarathne@gmail.com>
2021-06-07 20:12:17 +0530

    finish crud of research paper

--- BackEnd/src/controller/controller-researchpaper.js
+++ BackEnd/src/controller/controller-researchpaper.js
@@ -1,10 +1,9 @@
 const express = require('express')
 
 const router = express.Router()
-const { creteResearchPaper } = require('../service/service-researchpaper')
+const { creteResearchPaper, getAllResearchPaper, getSingleResearchPaper, deleteResearchPaper, updateResearchPaper } = require('../service/service-researchpaper')
 
 router.post("/create",async(req,res) => {
-
     try{
         let paper = req.body;
         const result = await creteResearchPaper(paper)
@@ -13,8 +12,53 @@ router.post("/create",async(req,res) => {
     }catch(err) {
         res.status(400).json({'eror': err})
     }
-    
-
 })
+
+router.get("/getall", async(req,res) => {
+    try {
+        const result = await getAllResearchPaper(req.query)
+        res.status(200).json({result})
+    } catch (err) {
+        res.status(400).json({err})
+    }
+})
+
+router.get("/get/:paperId", (req,res) => {
+    res.status(200).json(req.paper)
+})
+
+router.delete("/delete/:paperId", async(req,res) => {
+    try {
+        const result = await deleteResearchPaper(req.paper)
+        res.status(200).json({result})
+    } catch (err) {
+        res.status(400).json({err})
+    }
+})
+
+router.put("/update/:paperId", async(req,res) => {
+    try {
+        
+        const result = await updateResearchPaper(req.paper, req.body)
+        res.status(200).json({result})
+
+    } catch (err) {
+        res.status(400).json({err})
+    }
+})
+
+router.param("paperId", async(req,res,next,id) => {
+    try {
+        const result = await getSingleResearchPaper(id)
+        req.paper = result
+        next()
+    } catch (err) {
+        res.status(400).json({
+            'error': err
+        })
+    }
+})
+
+
 
 module.exports = router

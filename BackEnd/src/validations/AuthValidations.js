90460cf94765a6e145037cf733319419c14d9b63
pramitha1166 <pramitha.wanigarathne@gmail.com>
2021-06-26 13:32:53 +0530

    finish use register

--- BackEnd/src/validations/AuthValidations.js
+++ BackEnd/src/validations/AuthValidations.js
@@ -10,9 +10,9 @@ const joiUserSchema = Joi.object({
 
   password: Joi.string().min(4).max(1024).required(),
 
-  mobile: Joi.string().min(10).max(10).required(),
+  mobile: Joi.string().min(10).max(10),
 
-  profilePicture: Joi.string().uri().min(4).max(1024).required(),
+  profilePicture: Joi.string().uri().min(4).max(1024),
 
   role: Joi.string().alphanum().min(4).max(20).required(),
 });

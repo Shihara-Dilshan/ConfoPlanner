7d0ecb5ce44381d2e1739937f07c66dee4b09bdc
Dilhannadun <68120392+Dilhannadun@users.noreply.github.com>
2021-06-30 15:41:37 +0530

    retriewed pending shedules

--- BackEnd/src/controller/UserController.js
+++ BackEnd/src/controller/UserController.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { findUserById } = require('../util/SecurityConfig')
-const { getAllUsers } = require("../service/UserService")
-const viewUserNotifications = require('../service/NotificationService');
+const { findUserById } = require("../util/SecurityConfig");
+const { getAllUsers, updateRole } = require("../service/UserService");
+const viewUserNotifications = require("../service/NotificationService");
 
 router.get("/", (req, res) => {
-    res.send("sdsd");
+  res.send("sdsd");
 });
 
-router.get("/getall", async(req,res) => {
-    try {
-        let result = await getAllUsers()
-        res.status(200).json({result})
-    } catch (err) {
-        res.status(400).json({'error': err})
-    }
-})
+router.get("/getall", async (req, res) => {
+  try {
+    let result = await getAllUsers();
+    res.status(200).json({ result });
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
 
-router.get("/singleuser/:userId", (req,res) => {
-    res.json({'result': req.profile})
-})
+router.get("/singleuser/:userId", (req, res) => {
+  res.json({ result: req.profile });
+});
 
-router.param('userId', findUserById)
+router.param("userId", findUserById);
+
+router.patch("/:id", updateRole);
 
 module.exports = router;
--- BackEnd/src/service/UserService.js
+++ BackEnd/src/service/UserService.js
@@ -1,15 +1,28 @@
-'use strict'
+"use strict";
 
-const User = require('../model/User')
+const User = require("../model/User");
 
 exports.getAllUsers = () => {
-    return new Promise((resolve,reject) => {
-       User.find().exec((err,users) => {
-           if(err) {
-               reject(err)
-           }else {
-               resolve(users)
-           }
-       })
-    })
-}
+  return new Promise((resolve, reject) => {
+    User.find().exec((err, users) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(users);
+      }
+    });
+  });
+};
+
+exports.updateRole = async (req, res) => {
+  try {
+    if (req.body) {
+      await User.findByIdAndUpdate(req.params.id, {
+        $set: { role: req.body.role },
+      });
+      res.status(200).json({ msg: "sucess" });
+    }
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+};
